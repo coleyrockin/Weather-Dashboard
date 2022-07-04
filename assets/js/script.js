62094318691f1cd4805b6b41504539644a856177
coleyrockin <coleyrockin@aol.com>
2022-07-04 18:38:03 -0500

    Not working yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,6 +78,54 @@ function getResponseWeather(cityName){
         $("#today-weather").append(cityHumidity);
         var cityWindSpeed = $("<p>").text("Wind Speed: "+ response.wind.speed + " MPH");
         $("#today-weather").append(cityWindSpeed);
+        var CoordLon = response.coord.lon;
+        var CoordLat = response.coord.lat;
+        //api to get the UV index statistics
+         var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?appid="+ key+ "&lat=" + CoordLat +"&lon=" + CoordLon;
+         $.ajax({
+            url: queryURL2, method: "GET"
+         }).then(function(responseuv) {
+            var cityUV = $(("<span>")).text(responseuv.value);
+            var cityUVp = $(("<p>")).text("UV Index: ");
+            cityUVp.append(cityUV);
+            $("#today-weather").append(cityUV); 
+         })
+        })
+    };
 
-    }) 
-}
+    var queryURL3 = "https://api.openweathermap.org/data/25/forcast?q=" + cityName + "&appid=" + key;
+        $.ajax({
+            url: queryURL3,
+            method: "GET"
+        }).then(function(response5day) {
+            $("#boxes").empty();
+            console.log(response5day);
+            for(var i=0, j=0; j<=5; i=i+6){
+                var read_date = response5day.list[i].dt;
+                if(response5day.list[i].dt != response5day.list[i+1].dt){
+                    var FivedayDiv = $("<div>");
+                    FivedayDiv.attr("class","col-3 m-2 bg-primary")
+                    var d = new Date(0);
+                    d.setUTCSeconds(read_date);
+                    var date = d;
+                    console.log(date);
+                    var month = date.getMonth()+1;
+                    var day = date.getDate();
+                    var dayOutput = date.getFullYear() + '/' +
+                    (month<10 ? '0' : '') + month + '/' +
+                    (day<10 ? '0' : '') + day;
+                    var Fivedayh4 = $("<h6>").text(dayOutput);
+                    //Set src to the imags
+                    var imgtag = $("<img>");
+                    var skyconditions = response5day.list[i].weather[0].main;
+                    if(skyconditions==="Clouds"){
+                        imgtag.attr("src", "https://img.icons8.com/color/48/000000/cloud.png")
+                    } else if(skyconditions==="Clear"){
+                        imgtag.attr("src", "https://img.icons8.com/color/48/000000/summer.png")
+                    }else if(skyconditions==="Rain"){
+                        imgtag.attr("src", "https://img.icons8.com/color/48/000000/rain.png")
+                    }
+                }
+            }
+        })
+    
